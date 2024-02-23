79658e90b1cb2ae9094f7fb7f6cfb9f38b469220
Arun Gandham <arun.g@Technovert.com>
2024-02-23 12:51:28 +0530

    published

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
+app.UseHttpsRedirection(); 
 
 app.UseAuthorization();
 
